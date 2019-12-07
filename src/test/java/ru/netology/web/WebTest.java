26cbca2d8ac10b9318f3d0ec46f5aeb5b60f0efb
Tatiana Maliukova <skorobreshik@gmail.com>
2019-12-07 18:29:02 +0700

    Added second test with incorrect data

--- src/test/java/ru/netology/web/WebTest.java
+++ src/test/java/ru/netology/web/WebTest.java
@@ -8,14 +8,25 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class WebTest {
+//    @Test
+//    void shouldFullForm() {
+//        open("http://localhost:9999");
+//        $("[name='name']").setValue("Мамин-Сибиряк Иван Петрович");
+//        $("[name='phone']").setValue("+70000000000");
+//        $("[data-test-id='agreement']").click();
+//        $(byClassName("button")).click();
+//        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена!" +
+//                " Наш менеджер свяжется с вами в ближайшее время."));
+//    }
+
     @Test
-    void shouldFullForm() {
+    void shouldFullFormIncorrectly() {
         open("http://localhost:9999");
-        $("[name='name']").setValue("Мамин-Сибиряк Иван Петрович");
-        $("[name='phone']").setValue("+70000000000");
+        $("[name='name']").setValue("SSS");
+        $("[name='phone']").setValue("123");
         $("[data-test-id='agreement']").click();
         $(byClassName("button")).click();
-        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена!" +
-                " Наш менеджер свяжется с вами в ближайшее время."));
+        $(byClassName("input__sub")).shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только" +
+                " русские буквы, пробелы и дефисы."));
     }
 }
