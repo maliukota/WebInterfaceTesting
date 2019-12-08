9ca4ea4b96c12d4079b343dc3992b43e585cbee0
Tatiana Maliukova <skorobreshik@gmail.com>
2019-12-08 18:09:14 +0700

    United two tests

--- src/test/java/ru/netology/web/WebTest.java
+++ src/test/java/ru/netology/web/WebTest.java
@@ -8,16 +8,16 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class WebTest {
-//    @Test
-//    void shouldFullForm() {
-//        open("http://localhost:9999");
-//        $("[name='name']").setValue("Мамин-Сибиряк Иван Петрович");
-//        $("[name='phone']").setValue("+70000000000");
-//        $("[data-test-id='agreement']").click();
-//        $(byClassName("button")).click();
-//        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена!" +
-//                " Наш менеджер свяжется с вами в ближайшее время."));
-//    }
+    @Test
+    void shouldFullForm() {
+        open("http://localhost:9999");
+        $("[name='name']").setValue("Мамин-Сибиряк Иван Петрович");
+        $("[name='phone']").setValue("+70000000000");
+        $("[data-test-id='agreement']").click();
+        $(byClassName("button")).click();
+        $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена!" +
+                " Наш менеджер свяжется с вами в ближайшее время."));
+    }
 
     @Test
     void shouldFullFormIncorrectly() {
